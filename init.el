84649cf8e121737ca1ca4be89e1d29dc0b0572a4
Colin Matthias <cmatthias@gmail.com>
2014-01-27 21:18:41 -0500

    Update init.el with list of elpa packages, and some basic functionality to auto-install missing packages

--- init.el
+++ init.el
@@ -1,4 +1,19 @@
+(setq cem-packages
+      '(
+        better-defaults
+        go-mode
+        textmate
+       ))
 
-(require 'package)
+(package-initialize)
 (add-to-list 'package-archives
   '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(dolist (pkg cem-packages)
+  (when (and (not (package-installed-p pkg))
+             (assoc pkg package-archive-contents))
+    (package-install pkg)))
+
